a5d459f3d07b451e7a06974f27414cff0bcf73d7
宋俊杰 <eagle.song@cygia.com>
2019-06-26 14:57:01 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Hi {{author}}, Welcome to Your {{name}} project" />
   </div>
 </template>
 
